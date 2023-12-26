4b578a320454471ada78f2f1b68f4ecf60470bb3
rolandbailly <bailly.rol@gmail.com>
2023-12-26 19:36:08 +0100

    Shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import {Component, computed, inject, Signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {SavingFacade} from "./saving.facade";
-import {MatCardModule} from "@angular/material/card";
-import {CashFlowDetails} from "./model/saving.model";
-import {MatTableModule} from "@angular/material/table";
-import {BarChartComponent} from "./components/bar-chart/bar-chart.component";
-import {NgChartsModule} from "ng2-charts";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {LineChartComponent} from "./components/line-chart/line-chart.component";
+import {SideNavComponent} from "./components/side-nav/side-nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatCardModule, MatTableModule, NgChartsModule, BarChartComponent, MatToolbarModule, MatExpansionModule, LineChartComponent],
+  imports: [CommonModule, RouterOutlet, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   host: {ngSkipHydration: 'true'},
@@ -22,16 +14,4 @@ import {LineChartComponent} from "./components/line-chart/line-chart.component";
 })
 export class AppComponent {
 
-  protected savingFacade = inject(SavingFacade);
-  protected accounts = this.savingFacade.getAccounts();
-
-  protected totalAccount: Signal<number | undefined> = computed(() => {
-    const accounts = this.accounts();
-    return accounts?.map(account => account.comingBalance)
-      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-  });
-
-  protected cashFlows = this.savingFacade.getCashFlows(13);
-
-  protected cashFlowDetails: Signal<CashFlowDetails |undefined> = this.savingFacade.getCashFlowDetails();
 }
